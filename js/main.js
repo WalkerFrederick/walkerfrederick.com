6631d12680d5867a1ccf2098e1823934d7aad529
walker frederick <walker@urdev.xyz>
2018-09-26 13:27:13 -0400

    New UI and overall new codebase...still built on the HTML5 boilerplate.

---
+++ js/main.js
@@ -0,0 +1,42 @@
+let header = $('.full-screen');
+
+header.css('height', window.innerHeight);
+
+let menuClick = function () {
+  let menu = $('.links-list');
+  if (menu.css('display') === 'none') {
+    menu.css('display', 'block');
+    menu.css('width', '50%');
+    setTimeout(() => {},1000);
+    $('#menu-button').css('display', 'none');
+    setTimeout(() => {$('#menu-exit').css('display', 'block');},100);
+    menu.css('height',window.innerHeight);
+  }
+  else {
+    $('#menu-exit').css('display', 'none');
+    menu.css('width', '0%');
+    setTimeout(() => {menu.css('display', 'none'); },1000);
+    setTimeout(() => {$('#menu-button').css('display', 'block');},700);
+  }
+}
+
+function revertDesktop(x) {
+  if (x.matches) { // If media query matches
+
+    $('.links-list').css({'display':'flex','width':'100%'});
+    $('#menu-button').css('display', 'none');
+    $('.social-text').addClass('hidden');
+    $('.social-icon').removeClass('hidden');
+
+  }
+  else {
+    $('.links-list').css({'display':'none','width':'0%'});
+    $('#menu-button').css('display', 'block');
+    $('.social-icon').addClass('hidden');
+    $('.social-text').removeClass('hidden');
+  }
+}
+
+let x = window.matchMedia("(min-width: 900px)")
+revertDesktop(x); // Call listener function at run time
+x.addListener(revertDesktop); // Attach listener function on state changes
